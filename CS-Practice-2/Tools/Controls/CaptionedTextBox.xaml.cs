deac6703bcd5c7e82d6a5f755d2793ae6a4db388
M.K <maximus.khomenko@gmail.com>
2023-03-18 15:11:42 +0200

    + Implement UI design + Create custom object CaptionedTextBox + Begin implementing MVVM

---
+++ CS-Practice-2/Tools/Controls/CaptionedTextBox.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CS_Practice_2.Tools.Controls
+{
+    /// <summary>
+    /// Interaction logic for CaptionedTextBox.xaml
+    /// </summary>
+    public partial class CaptionedTextBox : UserControl
+    {
+        public string Caption
+        {
+            get { return TbCaption.Text; }
+            set { TbCaption.Text = value; }
+        }
+
+        public string Text
+        {
+            get { return TbValue.Text; }
+            set { TbValue.Text = value; }
+        }
+
+        public CaptionedTextBox()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ CS-Practice-2/Views/DataInputControl.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CS_Practice_2.Views
+{
+    /// <summary>
+    /// Interaction logic for DataInputControl.xaml
+    /// </summary>
+    public partial class DataInputControl : UserControl
+    {
+        public DataInputControl()
+        {
+            InitializeComponent();
+        }
+
+        private void BProceed_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BCancel_Click(object sender, RoutedEventArgs e)
+        {
+            Environment.Exit(0);
+        }
+    }
+}
